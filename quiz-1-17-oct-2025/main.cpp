70a844fd38af16d86428685b634c82e5e9ce6f3f
Charbel <charbel@harb.dev>
2025-10-17 15:34:12 +0300

    quiz 1

---
+++ quiz-1-17-oct-2025/main.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void display(int*, int);
+int get_sum(int*, int);
+double get_average(int*, int);
+int find_max(const int*, int);
+void sort_arr(int*, int);
+
+int main() {
+    int arr_size;
+    cout << "Enter size of array: \n";
+    cin >> arr_size;
+    int* arr = new int[arr_size];
+    for (int i = 0; i < arr_size; i++) {
+        cout << "Enter element #" << i + 1 << " : \n";
+        cin >> arr[i];
+    }
+    display(arr, arr_size);
+    int sum = get_sum(arr, arr_size);
+    cout << "The sum of all elements is " << sum << " \n";
+    double average = get_average(arr, arr_size);
+    cout << "The average of all element is " << average << " \n";
+    int max = find_max(arr, arr_size);
+    cout << "The max of all element is " << max << " \n";
+    sort_arr(arr, arr_size);
+    cout << "The sorted array is: \n";
+    display(arr, arr_size);
+    delete[] arr;
+    return 0;
+}
+
+void display(int* tab, int n) {
+    cout << "Printing array elements \n";
+    for (int i = 0; i < n; i++) {
+        cout << "Element # " << i+ 1 << " " << tab[i] << " \n";
+    }
+}
+
+int get_sum(int* tab, int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += tab[i];
+    }
+    return sum;
+}
+
+double get_average(int* tab, int n) {
+    int sum = get_sum(tab, n);
+    return static_cast<double>(sum) / n;
+}
+
+int find_max(const int* tab, int n) {
+    int max = tab[0];
+    for (int i = 1; i < n; i++) {
+        if (tab[i] > max) {
+            max = tab[i];
+        }
+    }
+    return max;
+}
+
+void sort_arr(int* arr, const int arr_size) {
+    // Insertion: Fix element 0
+    for (int i = 1; i < arr_size; i++) {
+        const int currentIteration = arr[i];
+        int previousIndex = i - 1;
+        while (previousIndex >= 0 && arr[previousIndex] > currentIteration) {
+            arr[previousIndex + 1] = arr[previousIndex];
+            previousIndex--;
+        }
+        arr[previousIndex + 1] = currentIteration;
+    }
+}
